3843a6d356cd52080c231fc0a6ed9b4b351ceca4
chfrchko5 <r4ger7@gmail.com>
2026-03-23 14:55:40 -0400

    more fixes and organizing

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ def guest_page():
         f.close()
     return render_template(guest_file, articles = articles)
 
+# an endpoint with <id> variable that takes the id of the article pressed on the site
+# opens up a new page with only that specific article
 @app.route("/article/<id>")
 def article(id):
     articles = []
@@ -52,37 +54,56 @@ def admin_page():
 
 # a page thats accessible from the admin page
 # used to add a new article into the articles.json file
-@app.route("/admin/new")
+@app.route("/admin/new", methods=["GET", "POST"])
 def new_article():
-    return render_template("admin_things/admin_new.html")
-
-@app.route("/admin/new", methods=["POST"])
-def create_new_article():
-    title = request.form["title"]
-    content = request.form["content"]
-
     with open(articles_json, 'r') as f:
         articles = json.load(f)
 
-    if articles:
-        next_id = max(id["id"] for id in articles) + 1
-    else:
-        next_id = 1
+    if request.method == "POST":
+        if articles:
+            next_id = max(id["id"] for id in articles) + 1
+        else:
+            next_id = 1
 
-    article = {
-        "id": next_id,
-        "title": title,
-        "date": str(datetime.datetime.now().strftime("%m-%d-%Y")),
-        "content": content
-    }
+        article = {
+            "id": next_id,
+            "title": request.form["title"],
+            "date": str(datetime.datetime.now().strftime("%m-%d-%Y")),
+            "content": request.form["content"]
+        }
 
-    articles.append(article)
+        articles.append(article)
 
-    with open(articles_json, 'w') as f:
-        json.dump(articles, f, indent=4)
+        with open(articles_json, 'w') as f:
+            json.dump(articles, f, indent=4)
+        
+        return {"message": "Article saved"}
 
-    return {"message": "Article saved"}
+    return render_template("admin_things/admin_new.html")
 
+@app.route("/admin/edit/<int:id>", methods=["GET", "POST"])
+def edit_article(id):
+    with open(articles_json, 'r') as f:
+        articles = json.load(f)
+
+    for article in articles:
+        if article['id'] == id:
+            if request.method == "POST":
+                article['title'] = request.form["new_title"]
+                article['content'] = request.form["new_content"]
+                article['date_edited'] = datetime.datetime.now().strftime("%m-%d-%Y")
+
+                with open(articles_json, 'w') as f:
+                    json.dump(articles, f, indent=4)
+
+                return {"message": "Article edited"}
+
+            return render_template(
+                "admin_things/admin_edit.html",
+                one_article=article
+            )
+
+    return "Article not found", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
